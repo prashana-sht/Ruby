d07848fc8381bd1b239c75770c67f73bcf0144db
prashana <shrestha.prashana@gmail.com>
2023-09-15 12:20:09 +0545

    Day-4

--- classes.rb
+++ classes.rb
@@ -10,28 +10,6 @@ puts dog.inspect
 cynthia = Dog.new("Cynthia", "Bulldog")
 puts cynthia.inspect
 
-#Assignment
-# Write a Car class with the following attributes:
-# Brand
-# Color
-# Maximum Passenger
-# Max Speed
-
-class Car
-    def initialize (brand, color, maxpassanger, maxspeed)
-        @brand = brand
-        @color = color
-        @maxpassanger = maxpassanger
-        @maxspeed = maxspeed
-    end
-end
-
-bmw = Car.new("bmw", "blue", "4", "100")
-puts bmw.inspect
-
-ford = Car.new("ford", "Gray", "6", "150")
-puts ford.inspect   
-
 
 #attr_reader and attr_writer
 
@@ -59,22 +37,100 @@ class Cat
   
   puts "After: " + cat.inspect
 
-# class Cat 
-#     def initialize(name, age)
-#         @name = name
-#         @age = age
-#     end
-#     def name = (name)
-#         @name = name
-#     end
-#     def age =(age)
-#         @age = age
-#     end
-#  #end
+#getter method
 
-# cat = Cat.new("cathy", 5)
-# puts "Before: " + cat.inspect
+class Student
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
 
-# cat.name = "Catherine"
-# cat.age = 4
-# puts " After: " + cat.inspect
+  def name=(name)
+    @name = name
+  end
+
+  def age=(age)
+    @age = age
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+end
+
+stud = Student.new("Prashana", 21)
+
+puts "Before: " + stud.inspect
+
+stud.name = "Rahul"
+stud.age = 22
+
+puts "After: " + stud.inspect
+
+puts "Name: #{stud.name}"
+puts "Age: #{stud.age}"
+
+#alternative way:
+
+class Person
+
+  # attr_writer :name, :age
+  # attr_reader :name, :age
+
+  attr_accessor :name, :age
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+per = Person.new("Ram", 18)
+
+puts per.name
+puts per.age
+
+per.name = "Shyam"
+per.age = 20
+
+puts per.name
+puts per.age
+
+
+#Assignment
+# Write a Car class with the following attributes:
+# Brand
+# Color
+# Maximum Passenger
+# Max Speed
+
+# Add both a getter method and setter method to each of the attributes 
+# in the Car class 
+class Car
+  def initialize (brand, color, maxpassanger, maxspeed)
+      @brand = brand
+      @color = color
+      @maxpassanger = maxpassanger
+      @maxspeed = maxspeed
+  end
+  attr_reader :brand, :color, :maxpassanger, :maxspeed
+  attr_writer :brand, :color, :maxpassanger, :maxspeed
+end
+
+bmw = Car.new("bmw", "blue", "4", "100")
+puts bmw.color
+
+bmw.brand = "BMW"
+bmw.maxspeed = "150"
+puts bmw.brand
+puts bmw.maxspeed
+
+# bmw = Car.new("bmw", "blue", "4", "100")
+# puts bmw.inspect
+
+# ford = Car.new("ford", "Gray", "6", "150")
+# puts ford.inspect   
