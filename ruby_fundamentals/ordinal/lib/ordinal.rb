40137ed1a4c6e65282021d20d2e2bd389971475a
prashana <shrestha.prashana@gmail.com>
2023-09-19 14:59:25 +0545

    new changes

--- ruby_fundamentals/ordinal/lib/ordinal.rb
+++ ruby_fundamentals/ordinal/lib/ordinal.rb
@@ -1,12 +1,19 @@
 def ordinal(number)
-  # your code here
-  if number == 1
-    number.to_s + "st"
-  elsif number == 2
-    number.to_s + "nd"
-  elsif number == 3
-    number.to_s + "rd"
-  else
+  if (number % 100) == 11 
     number.to_s + "th"
+  elsif (number % 100) == 12
+    number.to_s + "th"
+  elsif (number % 100) == 13
+    number.to_s + "th"
+  else
+    if (number % 10) == 1
+      number.to_s + "st"
+    elsif (number % 10 ) == 2
+      number.to_s + "nd"
+    elsif (number % 10) == 3
+      number.to_s + "rd"
+    else
+      number.to_s + "th"
+    end
   end
 end
